77776220bb5edbc42bb5aa998227f14602b8a16f
SougataMukherjee <49196315+SougataMukherjee@users.noreply.github.com>
2026-08-21 17:09:55 +0530

    add

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: `${import.meta.env.VITE_API_URL || "http://localhost:8080"}/api`,
+  baseURL: "https://nodejs-ecommerce-app-1.onrender.com/api",
   timeout: 10000,
 });
 
